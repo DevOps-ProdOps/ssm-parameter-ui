5fb671fd055fa55aa940dfe913965d80abe9c4f9
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2019-04-22 18:50:01 +0100

    Updates

---
+++ precache-manifest.e42bcd31d7ecc3e796e942de70f0ac5f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ssm-parameter-ui/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "10e139ec22f15019c4b4",
+    "url": "/ssm-parameter-ui/static/css/main.a3051c95.chunk.css"
+  },
+  {
+    "revision": "5526d3636616d89a7155",
+    "url": "/ssm-parameter-ui/static/js/runtime~main.5526d363.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ssm-parameter-ui/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "383fe71d575fdb7f81a5",
+    "url": "/ssm-parameter-ui/static/js/2.383fe71d.chunk.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ssm-parameter-ui/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ssm-parameter-ui/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ssm-parameter-ui/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ssm-parameter-ui/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ssm-parameter-ui/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ssm-parameter-ui/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "10e139ec22f15019c4b4",
+    "url": "/ssm-parameter-ui/static/js/main.10e139ec.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ssm-parameter-ui/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ssm-parameter-ui/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ssm-parameter-ui/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ssm-parameter-ui/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ssm-parameter-ui/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ssm-parameter-ui/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ssm-parameter-ui/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ssm-parameter-ui/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "383fe71d575fdb7f81a5",
+    "url": "/ssm-parameter-ui/static/css/2.3ebc57d6.chunk.css"
+  },
+  {
+    "revision": "ba0472ecbf5ac1a09f873d9f37745ef2",
+    "url": "/ssm-parameter-ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ssm-parameter-ui/precache-manifest.e42bcd31d7ecc3e796e942de70f0ac5f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ssm-parameter-ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
